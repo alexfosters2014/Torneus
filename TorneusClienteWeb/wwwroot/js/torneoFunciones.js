77ee2d98f70576a4f5f2b843429ac0bd7bbf06d2
proyectoort2023 <af245580@fi365.ort.edu.uy>
2023-09-10 10:51:15 -0300

    dev: scroll automatico ultima linea en notificaciones del organizador pronto

---
+++ TorneusClienteWeb/wwwroot/js/torneoFunciones.js
@@ -0,0 +1,17 @@
+﻿
+
+//window.ObtenerAnchoPantalla = () => {
+//    return window.innerWidth;
+//};
+
+//function getIdTokenFromFragment() {
+//    var fragment = window.location.hash.substr(1);
+//    var params = new URLSearchParams(fragment);
+//    var idToken = params.get("id_token");
+//    return idToken;
+//}
+
+function ScrollUltimaLinea() {
+    const notificacionContainer = document.getElementById("notificaciones-container");
+    notificacionContainer.scrollTop = notificacionContainer.scrollHeight;
+}
