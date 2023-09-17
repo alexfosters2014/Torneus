8437244ca10ec96e19f71a0aaa16a7b49256e531
proyectoort2023 <af245580@fi365.ort.edu.uy>
2023-09-17 09:57:10 -0300

    dev: asignacion al validador de usuario

--- Negocio/Validaciones/ValidadorUsuario.cs
+++ Negocio/Validaciones/ValidadorUsuario.cs
@@ -13,8 +13,10 @@ namespace Negocio.Validaciones
         public ValidadorUsuarioRegistro()
         {
             RuleFor(us => us.Mail).NotNull().NotEmpty().WithMessage("mail vacío");
-            RuleFor(us => us.Pass).NotEmpty().WithMessage("contrasela vacía");
+            RuleFor(us => us.Pass).NotEmpty().WithMessage("contraseña vacía");
+            RuleFor(us => us.Pass).MinimumLength(5).WithMessage("La contraseña debe tener al menos 5 caracteres");
             RuleFor(us => us.Rol).NotEmpty().NotNull().WithMessage("No ha seleccionado un rol");
+
 
         }
 
